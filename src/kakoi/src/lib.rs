c96c575c61ed9254d34c262d02b8c979b8aadc9b
Michael MacLeod <michaelmmacleod@gmail.com>
2021-03-03 18:46:30 -0800

    lib: add recursive_copy_instructor.rs

--- src/kakoi/src/lib.rs
+++ src/kakoi/src/lib.rs
@@ -5,4 +5,5 @@ pub mod copy_instructor;
 pub mod graph;
 pub mod graph_impl;
 pub mod index;
+pub mod recursive_copy_instructor;
 pub mod svg;
---
+++ src/kakoi/src/recursive_copy_instructor.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+pub mod test {
+    #[test]
+    fn test_0() {
+        assert_eq!(2 + 2, 4);
+    }
+}
